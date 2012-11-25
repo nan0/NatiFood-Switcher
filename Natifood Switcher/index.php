29a2d7d4ca87d44d8ac62e4ef9805ff554085b29
nan0 <nan0@nan0-Zenbook>
2012-11-25 15:13:26 +0000

    Commit website

---
+++ Natifood Switcher/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <script language="JavaScript1.2">
+            <!--
+            if (screen.width<=480) //if phone
+                window.location.replace("./mobile/")
+            else //if all else (tablet + pc)
+                window.location.replace("./website/")
+            //-->
+        </script>
+    </head>
+    <body>
+    </body>
+</html>
